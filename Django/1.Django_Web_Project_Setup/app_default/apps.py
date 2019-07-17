4fb4ecdfbe5039b7ba01f232858b4af75b170298
paullee714 <ba93love@gmail.com>
2019-07-18 01:20:25 +0900

    Django 구성 분석하기와 기본 세팅

---
+++ Django/1.Django_Web_Project_Setup/app_default/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppDefaultConfig(AppConfig):
+    name = 'app_default'
---
+++ Django/1.Django_Web_Project_Setup/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ibaul/Documents/GitHub/BlogCode/Django/1.Django_Web_Project_Setup/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
