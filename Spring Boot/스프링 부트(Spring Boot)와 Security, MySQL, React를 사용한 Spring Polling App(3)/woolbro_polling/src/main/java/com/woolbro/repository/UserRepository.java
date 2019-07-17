d7c4e28303a9b163d33c48b2e762fd77ed67ee32
paullee714 <ba93love@gmail.com>
2019-07-17 17:03:29 +0900

    스프링 부트(Spring Boot)와 Security, MySQL, React를 사용한 Spring Polling App(3)

---
+++ Spring Boot/스프링 부트(Spring Boot)와 Security, MySQL, React를 사용한 Spring Polling App(3)/woolbro_polling/src/main/java/com/woolbro/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.woolbro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.woolbro.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
