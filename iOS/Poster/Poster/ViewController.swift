ab3859db36417143cd7aeffe17272f5101cda671
paullee714 <ba93love@gmail.com>
2019-09-14 00:03:22 +0900

    Create Poster App(single view)

---
+++ iOS/Poster/Poster/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Poster
+//
+//  Created by 이바울 on 13/09/2019.
+//  Copyright © 2019 woolbro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func hello(){
+        print("Hello!")
+    }
+}
+
