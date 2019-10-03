a380f0782b9e755731a82aac50dd8129115bbb41
paullee714 <ba93love@gmail.com>
2019-10-03 14:43:40 +0900

    make view controller, add test alert button
    alert 생성

--- iOS/Poster/Poster/ViewController.swift
+++ iOS/Poster/Poster/ViewController.swift
@@ -17,6 +17,26 @@ class ViewController: UIViewController {
 
     @IBAction func hello(){
         print("Hello!")
+        
+        //add alert
+        let alert = UIAlertController(title: "hello alert", message: "firstApp!!", preferredStyle: .alert) // alert
+        let action = UIAlertAction(title: "ok", style: .default, handler: nil) // ok action
+        alert.addAction(action) //alert, action binding
+        
+        present(alert,animated: true,completion: nil)
+        
     }
+    @IBAction func newHello(){
+        print("this is new Hello!")
+        
+        //add alert
+        let testalert = UIAlertController(title: "hello alert", message: "new iphone 11!!", preferredStyle: .alert) // alert
+        let okaction = UIAlertAction(title: "ok", style: .default, handler: nil) // ok action
+        testalert.addAction(okaction) //alert, action binding
+        
+        present(testalert,animated: true,completion: nil)
+        
+    }
+
 }
 
