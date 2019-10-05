722f4dac0502f8afa77acbf54780e775dc99a38d
paullee714 <ba93love@gmail.com>
2019-10-05 20:29:25 +0900

    Make Poster Notification App(Basic Alert Func)

--- iOS/Poster/Poster/ViewController.swift
+++ iOS/Poster/Poster/ViewController.swift
@@ -9,34 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue:Int = 0 //변ㅅ
+    @IBOutlet weak var bountyLabel: UILabel! //레이블
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        refresh()
     }
-
-    @IBAction func hello(){
-        print("Hello!")
+    
+    @IBAction func showAlert(){
+        let message_value = "현상금은 $ \(currentValue) 입니다"
         
-        //add alert
-        let alert = UIAlertController(title: "hello alert", message: "firstApp!!", preferredStyle: .alert) // alert
-        let action = UIAlertAction(title: "ok", style: .default, handler: nil) // ok action
+        let alert = UIAlertController(title: "현상금 Alert", message: message_value, preferredStyle: .alert) // alert
+        let action = UIAlertAction(title: "ok", style: .default, handler: { action in
+            self.refresh()
+        }) // ok action
         alert.addAction(action) //alert, action binding
-        
         present(alert,animated: true,completion: nil)
         
-    }
-    @IBAction func newHello(){
-        print("this is new Hello!")
-        
-        //add alert
-        let testalert = UIAlertController(title: "hello alert", message: "new iphone 11!!", preferredStyle: .alert) // alert
-        let okaction = UIAlertAction(title: "ok", style: .default, handler: nil) // ok action
-        testalert.addAction(okaction) //alert, action binding
-        
-        present(testalert,animated: true,completion: nil)
         
     }
-
+    
+    func refresh(){
+        let randomNum = arc4random_uniform(1000000)+1
+        currentValue = Int(randomNum)
+        bountyLabel.text = "$ \(currentValue)"
+    }
 }
 
