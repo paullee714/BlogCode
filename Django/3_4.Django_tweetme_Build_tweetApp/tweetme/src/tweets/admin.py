4169f49caa7132859a9ad6df55ed4ea76d9ad313
paullee714 <ba93love@gmail.com>
2019-10-16 10:59:49 +0900

    tweet 앱 Build 및 Model 생성

---
+++ Django/3_4.Django_tweetme_Build_tweetApp/tweetme/src/tweets/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Tweet
+
+admin.site.register(Tweet)
+
+
---
+++ Django/3_4.Django_tweetme_Build_tweetApp/tweetme/src/tweets/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    content     = models.TextField()
+    
+    def __str__(self):
+        return str(self.content)
+    
+
+
+    
