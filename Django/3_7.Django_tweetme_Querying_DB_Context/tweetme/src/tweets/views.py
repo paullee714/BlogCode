030f7deaa83f62f16be3ec259a3146fd8d1ffc82
paullee714 <ba93love@gmail.com>
2019-10-18 02:10:25 +0900

    Tweet 앱 View의 querying database, context to html

---
+++ Django/3_7.Django_tweetme_Querying_DB_Context/tweetme/src/tweets/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from .models import Tweet
+# Create your views here.
+
+def tweet_detail_view(request, id=1):
+    obj = Tweet.objects.get(id=id) #GET from database
+    print(obj)
+    context ={
+        "object" :obj
+    }
+    return render(request, "tweets/detail_view.html",context)
+
+def tweet_list_view(request):
+    queryset = Tweet.objects.all()
+    print(queryset)
+    for obj in queryset:
+        print(obj.content)
+    context = {
+        "object_list" : queryset
+    }
+    return render(request, "tweets/list_view.html",context)
