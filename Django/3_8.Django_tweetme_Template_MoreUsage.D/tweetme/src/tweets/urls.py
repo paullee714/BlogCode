b70657dc099deee663dfead79d0fea0b89d6a7c5
paullee714 <ba93love@gmail.com>
2019-10-21 22:06:25 +0900

    Tweet App 쿼리 template 응용

---
+++ Django/3_8.Django_tweetme_Template_MoreUsage.D/tweetme/src/tweets/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from .views import tweet_detail_view, tweet_list_view
+
+urlpatterns = [
+    url(r'^$', tweet_list_view, name='list'),
+    url(r'^1/$', tweet_detail_view, name='detail')
+]
