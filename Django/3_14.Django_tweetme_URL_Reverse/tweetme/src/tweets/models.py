a70ee8f44f4d28ee500b573fcbe7af6bea44cae6
paullee714 <ba93love@gmail.com>
2019-10-30 22:54:36 +0900

    Tweet 앱 reverse lazy 적용 - success_url 대신 적용

---
+++ Django/3_14.Django_tweetme_URL_Reverse/tweetme/src/tweets/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.conf import settings
+from django.urls import reverse
+
+# Create your models here.
+
+class Tweet(models.Model):
+    
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content     = models.CharField(max_length=140)
+    timestamp   = models.DateTimeField(auto_now =True)
+    updated     = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.content)
+
+    def get_absolute_url(self):
+        return reverse("tweet:detail", kwargs={"pk": self.pk})
+    
+
+    def clean(self,*args,**kwargs):
+        content = self.content
+        if content == "바보":
+            raise ValidationError("저장 할 수 없는 단어  <" + content +" >  입니다.")
+        return super(Tweet,self).clean(*args,**kwargs)
---
+++ Django/3_14.Django_tweetme_URL_Reverse/tweetme/src/tweets/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.forms.utils import ErrorList
+from django import forms
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .mixins import FormUserNeededMixin, UserOwnerMixin
+from .models import Tweet
+from .forms import TweetModelForm
+# Create your views here.
+
+class TweetCreateView(FormUserNeededMixin,CreateView):
+    form_class = TweetModelForm
+    template_name = 'tweets/create_view.html'
+    # success_url = "/tweet/create/"
+    #success_url = reverse_lazy("tweet:detail")
+    #login_url = "/admin/login/"
+
+
+class TweetUpdateView(UserOwnerMixin, UpdateView):
+    queryset = Tweet.objects.all()
+    form_class = TweetModelForm
+    template_name = 'tweets/update_view.html'
+    #success_url = "/tweet/"
+
+
+class TweetDetailView(DetailView):
+    #template_name = "tweets/detail_view.html"
+    queryset = Tweet.objects.all()
+
+    def get_object(self):
+        print(self.kwargs)
+        pk = self.kwargs.get("pk")
+        obj = get_object_or_404(Tweet,pk=pk)
+        return Tweet.objects.get(id=pk)
+
+class TweetListView(ListView):
+    #template_name = "tweets/list_view.html"
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView,self).get_context_data(*args,**kwargs)
+        return context
+
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    success_url = reverse_lazy("tweet:list")
+    template_name = 'tweets/delete_confirm.html'
+
+
+
+
+# def tweet_create_view(request):
+#     form = TweetModelForm(request.POST or None)
+
+#     if form.is_valid():
+#         instance = form.save(commit = False)
+#         instance.user = request.user
+#         instance.save()
+#     context={
+#         "form" : form
+#     }
+#     return render(request, 'tweets/create_view.html',context)
+
+
+# def tweet_detail_view(request, id =1):
+#     obj = Tweet.objects.get(id=id) #GET from database
+#     print(obj)
+#     context ={
+#         "object" :obj
+#     }
+#     return render(request, "tweets/detail_view.html",context)
+
+# def tweet_list_view(request):
+#     queryset = Tweet.objects.all()
+#     print(queryset)
+#     for obj in queryset:
+#         print(obj.content)
+#     context = {
+#         "object_list" : queryset
+#     }
+#     return render(request, "tweets/list_view.html",context)
+
