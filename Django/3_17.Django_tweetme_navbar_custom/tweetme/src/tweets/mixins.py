79d97c720f9534c9b9eeaa487e40e2a58bba0a6d
paullee714 <ba93love@gmail.com>
2019-11-05 00:55:11 +0900

    Tweet 앱 Bootstrap Navbar 적용하기

---
+++ Django/3_17.Django_tweetme_navbar_custom/tweetme/src/tweets/mixins.py
@@ -0,0 +1,20 @@
+from django.forms.utils import ErrorList
+from django import forms
+
+
+class FormUserNeededMixin(object):
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:    
+            form.instance.user = self.request.user
+            return super(FormUserNeededMixin,self).form_valid(form)
+        else:
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["로그인 해야합니다."])
+            return self.form_invalid(form)
+
+class UserOwnerMixin(object):
+    def form_valid(self,form):
+        if form.instance.user == self.request.user:
+            return super(UserOwnerMixin, self).form_valid(form)
+        else :
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["로그인 사용자가 작성한 사용자와 다릅니다."])
+            return self.form_invalid(form)
